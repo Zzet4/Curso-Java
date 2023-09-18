2ded0b065a33d39d1bf6cb18000fde2ae1e94d48
Zzet4 <mendez4488@hotmail.com>
2023-09-18 16:15:00 +0200

    commit invocación de métodos.

---
+++ src/curso_java/ejercicios/A5_Metodos/A2_Invocacion_metodos_utils.java
@@ -0,0 +1,32 @@
+package curso_java.ejercicios.A5_Metodos;
+
+import curso_java.utilidades.Utils;
+
+public class A2_Invocacion_metodos_utils {
+	public static void main (String [] args) {
+		
+//		String contenidoMenu = "tu prima";
+//		int opcionMP = Utils.pintaMenu(contenidoMenu);
+		
+		
+		String nombre = Utils.scanString("Introduzca un nombre");
+		System.out.println(nombre);
+		
+		
+		int edad = Utils.scanInt("introduce tu edad");
+		System.out.println(edad);
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+
+
+}
