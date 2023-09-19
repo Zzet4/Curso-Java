86c85c4c292ef33888d4b73d02acfd91ff7ae824
Zzet4 <mendez4488@hotmail.com>
2023-09-19 20:57:26 +0200

    Commit definicion POO

---
+++ src/curso_java/teoria/A6_POO/A1_Programacion_Orientada_a_Objetos.java
@@ -0,0 +1,43 @@
+package curso_java.teoria.A6_POO;
+
+public class A1_Programacion_Orientada_a_Objetos {
+	public static void main (String [] args) {
+		
+		
+	/*
+	 * 
+	 * De momento vamos a entender una clase como un molde
+	 * El molde o clase elegida va a ser PERSONA
+	 * Esta clase PERSONA tiene una serie de ATRIBUTOS como son su nombre, apellido, dni, etc...
+	 * Además la clase PERSONA tiene una serie de METODOS como son comer, dormir, etc...
+	 * 
+	 * A partir de la clase o molde podemos crear los OBJETOS, a esta acción se la conoce como instanciar o instanciar un objeto
+	 * Los OBJETOS tienen los mismos ATRIBUTOS y los mismos METODOS.
+	 * 
+	 * 
+	 * 
+	 * TODOS LOS LENGUAJES ORIENTADOS A OBJETOS DEBEN TENER ESTAS 4 CARACTERÍSTICAS
+	 * 
+	 * 	-HERENCIA
+	 * 
+	 * 	-POLIMORFISMO
+	 * 
+	 * 	-ABSTRACCIÓN
+	 * 
+	 * 	-ENCAPSULACIÓN
+	 * 
+	 * 
+	 * 
+	 * 
+	 * */
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+
+}
