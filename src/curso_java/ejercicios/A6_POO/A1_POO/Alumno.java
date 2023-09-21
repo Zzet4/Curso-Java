29525b29048d6ba5e6a11ad1109b832d2e80109f
Zzet4 <mendez4488@hotmail.com>
2023-09-21 19:29:11 +0200

    Commit con encapsulacion

--- src/curso_java/ejercicios/A6_POO/A1_POO/Alumno.java
+++ src/curso_java/ejercicios/A6_POO/A1_POO/Alumno.java
@@ -1,13 +1,43 @@
 package curso_java.ejercicios.A6_POO.A1_POO;
 
 public class Alumno {
+	
+	
+	//A estas clases que no tienen main por que las usamos unicamente para definir las variables de instancia o Atributos, los constructores y 
+	//los métodos se las conoce como POJOs
+	//Posteriormente creamos una nueva clase con main llamada alumnoMain para crear los objetos y llamar a sus metodos.
 
-//Atributos
+//Atributos o Variables de instancia
+	//si declararamos estas variables como estáticas las convertiríamos en variables estáticas de clase y solo podrían ser accedidas por metodos 
+	//estátticos. Además todos los objetos de esta clase heredarían el valor de dicha variable estática y los cambios se verían reflejados en todos los
+	//objetos a la vez
 	String nombre;
 	String apellidos;
-	String dni;
+	private String dni;
 	int nota;
-
+//____________________________________________________________________________________________________________________________________________
+	//Declaramos el atributo dni como private para que no pueda ser modificado
+	//para poder acceder al método pero no modificarlo hay que crear un metodo aparte que sea set y otro para poder obtenerlo que sea get
+	//a este proceso le llamamos GETTER Y SETTER
+	
+	//Este metodo es el que habilita poder recoger el dato del dni
+	public void setDni (String dni) {
+	this.dni = dni;
+}
+	
+	//Este metodo es el que hay que usar para recogerlo.
+	public String getDni () {
+		return dni;
+	}
+	
+	//Para no tener que generar los getter y los setter de cada variable de instancia podemos usar boton derecho en la pantalla -> source ->
+	//Generate getter and setter -> y luego seleccionamos a que atributos se los queremos generar.
+	
+	//Vemos aqui el concepto de encapsulacion ya que un atributo declarado como private que no tenga definido el metodo set no puede ser modificado
+	//y si tampoco tiene habilitado el método get no podrá nisiquiera consultarse.
+	
+//_______________________________________________________________________________________________________________________________________________
+	
 //Constructores
 
 	// Vacío
@@ -30,13 +60,21 @@ public class Alumno {
 	}
 	
 	//Para no hacer tanto codigo igual, podemos sobrecargar los constructores
-	//igual que los métodos.
+	//como hacíamos con los métodos, sería así:
+	
+	
+	
+	
 
 //Métodos
+	//Este metodo no va a ser static ya que lo vamos a invocar con diferentes objetos, es decir, este metodo está orientado a objetos
+	// Si hicieramos este metodo static solo tendría acceso a las variables de la clase, a su vez estas deberían ser estáticas tambien.
+	//en ese caso el valor de esas variables es heredado para todos los objetos que se creen con esa clase, es decir, igual todos los objetos de
+	//la clase Persona se llamarían Javi si hubiéramos definido la variable nombre como static String nombre = javi.
 	public void estudiar() {
-		System.out.println("El alumno " + nombre);
+		System.out.println("El alumno " + this.nombre);
 		if (nota >= 5) {
-			switch (nota) {
+			switch (this.nota) {
 			case 5:
 
 			case 6:
@@ -53,11 +91,13 @@ public class Alumno {
 				break;
 
 			}
-		} else if (nota == 0) {
+		} else if (this.nota == 0) {
 			System.out.println("no ha estudiado nada");
 		} else {
 			System.out.println("ha estudiado poco");
 		}
 	}
+	
+	//DENTRO DE LAS CLASES POJO NOOOO DEBE HABER MÉTODOS STATIC, DE HECHO, LOS ÚNICOS METODOS STATIC QUE DEBE HABER SON LOS DE LAS UTILIDADES.
 
 }
--- src/curso_java/ejercicios/A6_POO/A1_POO/AlumnoMain.java
+++ src/curso_java/ejercicios/A6_POO/A1_POO/AlumnoMain.java
@@ -5,59 +5,81 @@ import curso_java.utilidades.Utils;
 public class AlumnoMain {
 	public static void main (String [] args) {
 		
+//En esta clase alumnoMain camos a crear los objetos dentro del metodo main para poder invocar a sus métodos.
 		
 		
 //CREAMOS LOS OBJETOS
 		
 		//Creamos un objeto con variable a1 desde el constructor vacío
-		Alumno a1 = new Alumno();
-			a1.nombre = "fulanito";
-			a1.apellidos = "de tal";
-			a1.dni = "0000A";
-			a1.nota = 10;
-			
+		Alumno alumno1 = new Alumno();
+		alumno1.nombre = "fulanito";
+		alumno1.apellidos = "de tal";
+		alumno1.setDni("0000A");
+		alumno1.nota = 10;
+		
+	
 			
 			
 		//Creamos un objeto con variable a1 desde el constructor
-		//de nombre, apellido, dni
-		Alumno a2 = new Alumno("Paco", "Sanchez", "1111B");
+		Alumno alumno2 = new Alumno("Paco", "Sanchez", "1111B");
+		
+		//de nombre, apellido, dni. Como este objeto ha sido creado mediante un constructor que no contempla la nota
+		//por defecto Paco tendrá un 0
 		
 		
 		
 		//Creamos un objeto con variable a1 desde el constructor 
 		//de nombre, apellido, dni y nota
-		Alumno a3 = new Alumno("Jose", "Lopez", "2222C", 6);
+		Alumno alumno3 = new Alumno("Jose", "Lopez", "2222C", 6);
 		
 		
 //LLAMAMOS A L METODO PARA CADA OBJETO		
 		
 		//llamamos al metodo estudiar para cada objeto
-		a1.estudiar();
-		a2.estudiar();
-		a3.estudiar();
+		alumno1.estudiar();
+		alumno2.estudiar();
+		alumno3.estudiar();
 		
 		
 	
 //GUARDAMOS TODOS LOS ALMUNOS EN UN ARRAY Y MOSTRAMOS LOS APROBADOS.
 		
-		//creamos un array y metemos a los almunos
+		//creamos un array y metemos a los almunos.
+		//OJO, si nos fijamos el tipo del array es Alumno ya que los objetos que va a contener son del "tipo" Alumno, (su clase).
 		
-		int numAlumnos = Utils.scanInt("¿Cuantos alumnos hay ?");
-		Alumno [] alumnos = {a1,a2,a3};
+		Alumno [] alumnos = {alumno1,alumno2,alumno3};
 		
 		
 		//Sacamos solo los aprobados.
 		System.out.println("Los alumnos aprobados son: ");
-		for ( int i=0 ; i<numAlumnos  ; i++ ) {
+		for ( int i=0 ; i<alumnos.length  ; i++ ) {
 				if (alumnos[i].nota>=5) {
 						System.out.println(alumnos[i].nombre);	
 				}	
 			}
+		
+		//Si queremos llamar al método pintaAprobados creado un poco más abajo necesitaremos crear un objeto ya que dicho método no es estático.
+		//el objeto que creamos debe pertenecer a la misma clase a la que pertenece el método ya que cada objeto tendrá las funciones de su clase
+		//es por esto que vamos a llamar al objeto que pintara los aprobados, profesor
+		
+		AlumnoMain profesor = new AlumnoMain();//usamos el constructor por defecto
+		profesor.pintaAprobados(alumnos);//Le decimos al objeto de esta clase que invoque al metodo (no static) de esta misma clase.
+											//notese que el parametro que le pasamos al método es el array de alumnos que necesita.
+		
 			
-		}
-		
-		
 	}
+		
+	//En este punto podríamos meter la funcionalidad de pintarAprobados en un método para facilitar el código a futuro.
+	//Ojo, fuera del main
+	public void pintaAprobados (Alumno [] alumnos) {
+		System.out.println("Los alumnos aprobados son: ");
+		for ( int i=0 ; i<alumnos.length  ; i++ ) {
+				if (alumnos[i].nota>=5) {
+						System.out.println(alumnos[i].nombre);	
+				}	
+			}
+		}
+}
 	
 	
 	
